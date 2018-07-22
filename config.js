2720c148aa9310014f9c16c7cd8a690c661496c2
abhinash <abhi@macs-MacBook.local>
2018-07-21 21:12:17 -0500

    chat app

---
+++ config.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/6c328ebd-b3cb-4f32-9598-12c8ceac2f78/token";
+const instanceLocator = "v1:us1:6c328ebd-b3cb-4f32-9598-12c8ceac2f78";
+
+export { tokenUrl, instanceLocator }
